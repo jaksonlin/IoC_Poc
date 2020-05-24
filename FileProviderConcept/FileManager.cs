5a2ca508810a0979c2db1f8b791489a078e6d685
Zack Lam <zhislin@icloud.com>
2020-05-25 00:33:33 +0800

    fileprovider

---
+++ FileProviderConcept/FileManager.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileProviderConcept
+{
+    public class FileManager : IFileManager
+    {
+        private readonly IFileProvider fileProvider;
+        public FileManager(IFileProvider fileProvider) => this.fileProvider = fileProvider;
+
+        public void ShowStructure(Action<int, string> render)
+        {
+            int indent = -1;
+            Render("");
+            void Render(string subPath)
+            {
+                indent += 1;
+                foreach(var fileInfo in this.fileProvider.GetDirectoryContents(subPath))
+                {
+                    render(indent, fileInfo.Name);
+                    if (fileInfo.IsDirectory)
+                    {
+                        Render($@"{subPath}\{fileInfo.Name}".TrimStart('\\'));
+                    }
+                }
+                indent -= 1;
+            }
+        }
+
+        public async Task<String> ReadAllTextAsync(String path)
+        {
+            byte[] buffer;
+            using(var stream = this.fileProvider.GetFileInfo(path).CreateReadStream())
+            {
+                buffer = new byte[stream.Length];
+                await stream.ReadAsync(buffer, 0, buffer.Length);
+            }
+            return Encoding.Default.GetString(buffer);
+        }
+    }
+}
---
+++ FileProviderConcept/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileProviderConcept
+{
+    public interface IFileManager
+    {
+        void ShowStructure(Action<int, string> render);
+        Task<String> ReadAllTextAsync(String path);
+    }
+
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            static void Print(int layer, string name) => Console.WriteLine($@"{new string(' ', layer * 4)}{name}");
+            var container = new ServiceCollection()
+                .AddSingleton<IFileProvider>(new PhysicalFileProvider(@"f:\checktest"))
+                //.AddSingleton<IFileProvider>(new EmbeddedFileProvider(Assembly.GetEntryAssembly()))
+                .AddSingleton<IFileManager, FileManager>()
+                .BuildServiceProvider();
+            using (var sc = container.CreateScope())
+            {
+                var fm = sc.ServiceProvider.GetRequiredService<IFileManager>();
+                fm.ShowStructure(Print);
+                var content = await fm.ReadAllTextAsync("test02.py");
+                //var content = await fm.ReadAllTextAsync("test_emb.txt");
+                Console.WriteLine(content);
+
+            }
+            Console.ReadLine();
+        }
+
+        static async Task FsWatcherFromProvider()
+        {
+            using (var fileProvider = new PhysicalFileProvider(@"f:\checktest")) {
+                string origin = null;
+                ChangToken.OnChange(() => fileProvider.Watch("test.txt"), CallBack);
+                while (true)
+                {
+                    File.WriteAllText(@"f:\checktest\test.txt", DateTime.Now.ToString());
+                    await Task.Delay(5000);
+
+                }
+                async void CallBack()
+                {
+                    using (var stream = fileProvider.GetFileInfo("test.txt").CreateReadStream())
+                    {
+                        var buffer = new byte[stream.Length];
+                        await stream.ReadAsync(buffer, 0, buffer.Length);
+                        string current = Encoding.Default.GetString(buffer);
+                        if(current != origin)
+                        {
+                            Console.WriteLine("oringal != current");
+                            origin = current;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
